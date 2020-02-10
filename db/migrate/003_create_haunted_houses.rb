46195d8c03120c635ab6eb5862034e82b68b8476
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-02-10 11:24:00 -0800

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouse < ActiveRecord::Migration[4.2]
+    def change
+      create_table :haunted_houses do |t|
+        t.string :name
+        t.integer :price
+        t.string :size
+        t.text :image_url
+      end
+    end
+  end
